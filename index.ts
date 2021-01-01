611f4b0fb55b803bd75f7facdb32e2f9437e21a2
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-01 19:18:15 +0100

    UI: Remember display value when hiding

--- index.ts
+++ index.ts
@@ -404,6 +404,7 @@ type Elements = {
 
 class UI {
   elements: Elements
+  _display: { [key: string]: string }
 
   constructor() {
     this.elements = {
@@ -412,16 +413,24 @@ class UI {
       newGameMenu: getElement('new-game-menu'),
       finishGameMenu: getElement('finish-game-menu'),
       finishGameCode: getElement('finish-game-code'),
-      newButton: getElement('new'),
+      newButton1: getElement('new1'),
       showButton: getElement('show'),
+      header: getElement('header'),
     }
+    this._display = {}
   }
 
   show(key: keyof Elements) {
-    this.elements[key].style.display = 'block'
+    console.log('hsow', this._display)
+
+    this.elements[key].style.display = this._display[key] || 'block'
   }
 
   hide(key: keyof Elements) {
+    this._display[key] = window.getComputedStyle(
+      this.elements[key],
+      null,
+    ).display
     this.elements[key].style.display = 'none'
   }
 }
