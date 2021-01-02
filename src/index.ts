b2617f812b5393c899ef99713928d9e2a9264a71
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-02 12:27:42 +0100

    Extract circle into separate file

--- src/index.ts
+++ src/index.ts
@@ -49,57 +49,6 @@ type GameConfig = {
   symbolGenerator: SymbolGenerator
 }
 
-class Circle {
-  ctx: CanvasRenderingContext2D
-  _object: Path2D | void
-  centerX: number
-  centerY: number
-  text: string
-  color: string
-
-  constructor(
-    ctx: CanvasRenderingContext2D,
-    centerX: number,
-    centerY: number,
-    text: string,
-    color: string,
-  ) {
-    this.ctx = ctx
-    this._object = undefined
-    this.centerX = centerX
-    this.centerY = centerY
-    this.text = text
-    this.color = color
-  }
-
-  draw(hideNumbers: boolean) {
-    this._object = new Path2D()
-    this.ctx.beginPath()
-    this._object.arc(this.centerX, this.centerY, RADIUS, 0, 2 * Math.PI, false)
-    this.ctx.fillStyle = this.color
-    this.ctx.fill(this._object)
-    this.ctx.lineWidth = 2
-    this.ctx.strokeStyle = '#003300'
-    this.ctx.stroke(this._object)
-
-    this.ctx.fillStyle = 'black'
-
-    if (!hideNumbers) {
-      this.ctx.font = `${FONTSIZE}px sans-serif`
-      this.ctx.textBaseline = 'middle'
-      this.ctx.textAlign = 'center'
-      this.ctx.fillText(this.text, this.centerX, this.centerY)
-    }
-  }
-
-  isInPath(x: number, y: number) {
-    if (!this._object) {
-      throw new Error('path is undefined')
-    }
-    return this.ctx.isPointInPath(this._object, x, y)
-  }
-}
-
 class Targets {
   private _targets: Circle[]
   constructor() {
