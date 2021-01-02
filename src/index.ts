87509a9f5015ba0ce8ab7093a8e71a20486d8236
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-02 12:14:23 +0100

    Move index.ts to src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -40,68 +40,6 @@ function getElement(className: string) {
   return el as HTMLElement
 }
 
-class Stats {
-  start: number
-  end: number
-  startCurrent: number
-  clicks: number
-  correctClicks: number
-  intervals: { number: string; duration: number }[]
-
-  constructor() {
-    const now = getCurrentTimestamp()
-    this.start = now
-    this.end = now
-    this.startCurrent = now
-    this.clicks = 0
-    this.correctClicks = 0
-    this.intervals = []
-  }
-
-  click() {
-    this.clicks += 1
-  }
-
-  foundNumber(number: string) {
-    this.correctClicks += 1
-    const now = getCurrentTimestamp()
-    this.intervals.push({ number, duration: now - this.startCurrent })
-    this.startCurrent = now
-  }
-
-  finish() {
-    this.end = getCurrentTimestamp()
-  }
-
-  print() {
-    let res = ''
-
-    res += `
-Total duraction: ${(this!.end - this.start) / 1000} sec
-Misclicks: ${this.clicks - this.correctClicks}
-
-`
-
-    for (const int of this.intervals) {
-      res += `${int.number}: ${int.duration / 1000} sec\n`
-    }
-
-    const opts = {
-      height: 6,
-      format: function (x: number, i: number) {
-        return (x / 1000).toFixed(2)
-      },
-    }
-    // console.log(
-    //   asciichart.plot(
-    //     this.intervals.map((x) => x.duration),
-    //     opts,
-    //   ),
-    // )
-    return res
-  }
-}
-
 class Timers {
   _timers: Set<number>
 
@@ -640,12 +578,12 @@ function main() {
   ui.hide('header')
   ui.elements.newButton1.addEventListener('click', () => {
     const gameConfig: GameConfig = {
-      amount: 2,
+      amount: 10,
       addNumberOnMisclick: true,
       autoAddNumberInterval: 5,
-      hideNumbersAfter: 3,
+      hideNumbersAfter: 0,
       hideAfterFirstClick: true,
-      symbolGenerator: new MixAsc(),
+      symbolGenerator: new NumericAsc(),
     }
 
     timers.clearAll()
