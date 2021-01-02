cd786806c867de718bafc337db923bf8378c137c
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-02 12:29:00 +0100

    Extract UI into separate file

--- src/index.ts
+++ src/index.ts
@@ -75,39 +75,6 @@ type Elements = {
   header: HTMLElement
 }
 
-class UI {
-  elements: Elements
-  _display: { [key: string]: string }
-
-  constructor() {
-    this.elements = {
-      canvasWrapper: getElement('canvas-wrapper'),
-      canvas: getElement('canvas') as HTMLCanvasElement,
-      newGameMenu: getElement('new-game-menu'),
-      finishGameMenu: getElement('finish-game-menu'),
-      finishGameCode: getElement('finish-game-code'),
-      newButton1: getElement('new1'),
-      showButton: getElement('show'),
-      header: getElement('header'),
-    }
-    this._display = {}
-  }
-
-  show(key: keyof Elements) {
-    console.log('hsow', this._display)
-
-    this.elements[key].style.display = this._display[key] || 'block'
-  }
-
-  hide(key: keyof Elements) {
-    this._display[key] = window.getComputedStyle(
-      this.elements[key],
-      null,
-    ).display
-    this.elements[key].style.display = 'none'
-  }
-}
-
 class Board {
   ctx: CanvasRenderingContext2D
   targets: Targets
