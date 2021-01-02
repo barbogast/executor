529f4fa38effb118e5a27fff461053df73a178d4
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-02 12:30:28 +0100

    Extract Game into separate file

---
+++ src/game.ts
@@ -0,0 +1,94 @@
+class Game {
+  targets: Targets
+  board: Board
+  stats: Stats
+  ui: UI
+  gameConfig: GameConfig
+
+  constructor(ui: UI, board: Board, targets: Targets, gameConfig: GameConfig) {
+    this.stats = new Stats()
+    this.board = board
+    this.ui = ui
+    this.targets = targets
+    this.gameConfig = gameConfig
+  }
+
+  start() {
+    this.ui.show('header')
+    this.board.clear()
+
+    COLORS.sort(() => 0.5 - Math.random())
+
+    this.board.setup()
+
+    for (let i = 0; i < this.gameConfig.amount; i++) {
+      this.addNumber()
+    }
+    this.board.draw()
+
+    this.board.registerOnClickHandler((circle) => this.onClick(circle))
+
+    this.ui.show('showButton')
+    this.ui.elements.showButton.addEventListener('click', () => {
+      this.addNumber()
+      this.board.setNumberVisibility(true, 0)
+      this.board.setNumberVisibility(false, 2)
+    })
+
+    if (this.gameConfig.hideNumbersAfter) {
+      this.board.setNumberVisibility(false, this.gameConfig.hideNumbersAfter)
+    }
+
+    if (this.gameConfig.autoAddNumberInterval) {
+      timers.setInterval(() => {
+        this.addNumber()
+        this.board.draw()
+      }, this.gameConfig.autoAddNumberInterval * 1000)
+    }
+  }
+
+  addNumber() {
+    const [centerX, centerY] = this.board.findFreeSpot()
+    if (this.gameConfig.symbolGenerator.isLast()) {
+      return
+    }
+    const nextNumber = this.gameConfig.symbolGenerator.next()
+    this.targets.add(
+      new Circle(
+        this.board.getContext(),
+        centerX,
+        centerY,
+        String(nextNumber),
+        this.gameConfig.symbolGenerator.getColor(),
+      ),
+    )
+  }
+
+  onClick(target: Circle | void) {
+    this.stats.click()
+    if (this.gameConfig.hideAfterFirstClick) {
+      this.board.setNumberVisibility(false, 0)
+    }
+    if (!target) {
+      if (this.gameConfig.addNumberOnMisclick) {
+        this.addNumber()
+        this.board.draw()
+      }
+      return
+    }
+
+    const targetIsCurrent = this.targets.tapTarget(target)
+    if (targetIsCurrent) {
+      this.stats.foundNumber(target.text as string)
+      if (this.targets.allTargetsReached()) {
+        this.stats.finish()
+        this.ui.hide('canvasWrapper')
+        this.ui.show('finishGameMenu')
+        this.ui.hide('showButton')
+        this.ui.elements.finishGameCode.innerHTML = this.stats.print()
+        timers.clearAll()
+      }
+      this.board.draw()
+    }
+  }
+}
