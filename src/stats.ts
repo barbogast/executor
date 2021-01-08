420c6205e33dfc277e5ada118cb9c864acd603d4
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-08 04:15:40 +0100

    Stats: Add types; add more data to export

--- src/stats.ts
+++ src/stats.ts
@@ -2,13 +2,46 @@ function getCurrentTimestamp() {
   return new Date().getTime()
 }
 
+type Interval = { duration: number; number: string }
+type GameStats = {
+  gameConfig: GameConfig
+  intervals: Interval[]
+  stats: {
+    start: number
+    end: number
+    clicks: number
+    correctClicks: number
+  }
+}
+
+type LocalStorage = {
+  games: {
+    [key in GameType]: GameStats[]
+  }
+}
+
+// https://stackoverflow.com/a/45309555
+function median(values: number[]) {
+  if (values.length === 0) return 0
+
+  values = [...values].sort(function (a, b) {
+    return a - b
+  })
+
+  const half = Math.floor(values.length / 2)
+
+  if (values.length % 2) return values[half]
+
+  return (values[half - 1] + values[half]) / 2.0
+}
+
 class Stats {
   start: number
   end: number
   startCurrent: number
   clicks: number
   correctClicks: number
-  intervals: { number: string; duration: number }[]
+  intervals: Interval[]
   gameConfig: GameConfig
 
   constructor(gameConfig: GameConfig) {
@@ -23,18 +56,50 @@ class Stats {
   }
 
   static statsToCsv() {
-    const columns = ['start', 'end', 'clicks', 'correctClicks']
+    const columns = [
+      'gameConfig.gameType',
+      'gameConfig.difficulty',
+      'stats.start',
+      'stats.end',
+      'stats.clicks',
+      'stats.correctClicks',
+      'intervals.average',
+      'intervals.median',
+      'intervals.min',
+      'intervals.max',
+    ]
     const s = localStorage.getItem('stats')
     if (!s) {
       return ''
     }
 
-    const stats = JSON.parse(s)
-    let output = ''
+    const stats = JSON.parse(s) as LocalStorage
+    let output = columns.map((s) => s.split('.')[1]).join(';') + '\n'
     for (const [gameType, games] of Object.entries(stats.games)) {
-      for (const game of games as Array<{ stats: { [key: string]: string } }>) {
+      for (const game of games) {
         for (const col of columns) {
-          output += game.stats[col] + ';'
+          const [obj, prop] = col.split('.')
+
+          let value
+          if (obj === 'intervals') {
+            if (prop === 'average') {
+              value = (
+                game.intervals.reduce((a, b) => a + b.duration, 0) /
+                game.intervals.length
+              ).toFixed(1)
+            } else if (prop === 'max') {
+              value = Math.max(...game.intervals.map((i) => i.duration))
+            } else if (prop === 'min') {
+              value = Math.min(...game.intervals.map((i) => i.duration))
+            } else if (prop === 'median') {
+              value = median(game.intervals.map((i) => i.duration))
+            }
+          } else {
+            // @ts-ignore
+            value = game[obj][prop]
+          }
+
+          output += value + ';'
         }
         output += '\n'
       }
@@ -56,13 +121,13 @@ class Stats {
 
   store() {
     const s = localStorage.getItem('stats')
-    const currentStats = s ? JSON.parse(s) : { games: {} }
+    const currentStats: LocalStorage = s ? JSON.parse(s) : { games: {} }
     const gameType = this.gameConfig.gameType
     if (!currentStats.games[gameType]) {
       currentStats.games[gameType] = []
     }
 
-    currentStats.games[gameType || 'unkownType'].push({
+    const newEntry: GameStats = {
       gameConfig: this.gameConfig,
       stats: {
         start: this.start,
@@ -71,7 +136,9 @@ class Stats {
         correctClicks: this.correctClicks,
       },
       intervals: this.intervals,
-    })
+    }
+
+    currentStats.games[gameType || 'unkownType'].push(newEntry)
     localStorage.setItem('stats', JSON.stringify(currentStats))
   }
 
